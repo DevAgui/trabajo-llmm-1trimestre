9eaa56b19c3e433d2ec4df1ff9674db0e17ca2cc
Diego Aguilera Martín <91873599+DiegoAguileraMartin@users.noreply.github.com>
2021-12-12 14:17:24 +0100

    Delete javascripts directory

--- javascripts/javascript.js
+++
@@ -1,9 +0,0 @@
-/* BARRA DE NAVEGACION RESPONSIVE*/
-function myFunction() {
-    var x = document.getElementById("topnav");
-    if (x.className === "myTopnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "myTopnav";
-    }
-  }
