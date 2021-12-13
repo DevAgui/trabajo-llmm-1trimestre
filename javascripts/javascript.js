1b0526d8407b7b199fe76959ba39cc9114e02de1
Diego Aguilera Martín <91873599+DiegoAguileraMartin@users.noreply.github.com>
2021-12-13 19:12:01 +0100

    Add files via upload

---
+++ javascripts/javascript.js
@@ -0,0 +1,9 @@
+/* responsive navigation bar */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+  } else {
+      x.className = "topnav";
+  }
+}
